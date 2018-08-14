ef83d2c92786f21da65bd9f682990e14a6f281ec
leagurru <leagurru@gmail.com>
2018-08-14 12:28:14 -0300

    Clases 72

---
+++ class_constructor.php
@@ -0,0 +1,18 @@
+<?php
+
+class Car {
+
+    var $ruedas ;
+    var $color = "Gris Artense";
+    var $puertas = 4 ;
+
+    function __construct(){
+        echo $this->ruedas = 4;
+    }
+
+}
+
+$bmw = new Car();
+
+
+?>
